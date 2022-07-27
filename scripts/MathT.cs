d012d01b443f60a3c080868c8131399d21b15ef2
Thomas SURBON <t.surbon@student.isartdigital.com>
2022-07-27 07:08:07 -0400

    feat: add methods Normalized, IsNormalized and Angle in struct Vector2

--- scripts/MathT.cs
+++ scripts/MathT.cs
@@ -18,15 +18,13 @@ namespace Com.Surbon.CSUtils
 				y = pY;
 			}
 
-			public float Length()
-			{
-				return MathF.Sqrt(x * x + y * y);
-			}
+			public float Angle() => MathF.Atan2(y, x);
 
-			public float LengthSquared()
-			{
-				return x * x + y * y;
-			}
+			public bool IsNormalized() => LengthSquared() == 1f;
+
+			public float Length() => MathF.Sqrt(x * x + y * y);
+
+			public float LengthSquared() => x * x + y * y;
 
 			public void Normalize(float length = 1)
 			{
@@ -38,6 +36,17 @@ namespace Com.Surbon.CSUtils
 					x /= l;
 					y /= l;
 				}
+			}
+
+			public Vector2 Normalized()
+			{
+				float l = LengthSquared();
+
+				if (l == 0)
+					throw new InvalidOperationException("The vector's length must be greater than 0");
+
+				l = MathF.Sqrt(l);
+				return new Vector2(x / l, y / l);
 			}
 		}
 	}
