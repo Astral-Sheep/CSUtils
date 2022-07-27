743165e32cf559bb640833d4dd7a4b73544e95de
Thomas SURBON <t.surbon@student.isartdigital.com>
2022-07-27 15:55:40 -0400

    doc: add doc in class MathT

--- scripts/MathT.cs
+++ scripts/MathT.cs
@@ -3,12 +3,16 @@
 namespace Com.Surbon.CSUtils
 {
 	/// <summary>
-	/// 
+	/// Provides structs and methods for mathematical operations
 	/// </summary>
 	public static class MathT
 	{
 		#pragma warning disable CS0659 // Le type se substitue à Object.Equals(object o) mais pas à Object.GetHashCode()
 		#pragma warning disable CS0661 // Le type définit l'opérateur == ou l'opérateur != mais ne se substitue pas à Object.GetHashCode()
+
+		/// <summary>
+		/// Representation of a vector in a 2 dimensional space.
+		/// </summary>
 		public struct Vector2
 		{
 			public float x;
@@ -42,14 +46,29 @@ namespace Com.Surbon.CSUtils
 
 			#endregion OPERATORS
 
+			/// <summary>
+			/// Returns the vector with absolute values.
+			/// </summary>
 			public Vector2 Abs() => new Vector2(MathF.Abs(x), MathF.Abs(y));
 
+			/// <summary>
+			/// Returns the vector's angle in radians.
+			/// </summary>
 			public float Angle() => MathF.Atan2(y, x);
 
+			/// <summary>
+			/// Returns the angle between the vector (this) and the given vector.
+			/// </summary>
 			public float AngleTo(Vector2 vector) => MathF.Atan2(Cross(vector), Dot(vector));
 
+			/// <summary>
+			/// Returns the angle between the line passing through both points, and the x axis.
+			/// </summary>
 			public float AngleToPoint(Vector2 vector) => (vector - this).Angle();
 
+			/// <summary>
+			/// Rounds up the length of the vector.
+			/// </summary>
 			public void CeilLength()
 			{
 				float l = x * x + y * y;
@@ -62,12 +81,18 @@ namespace Com.Surbon.CSUtils
 				}
 			}
 
+			/// <summary>
+			/// Rounds up the values of the vector.
+			/// </summary>
 			public void CeilValues()
 			{
 				x = MathF.Ceiling(x);
 				y = MathF.Ceiling(y);
 			}
 
+			/// <summary>
+			/// Sets the length of the vector between min and max.
+			/// </summary>
 			public void ClampLength(float min, float max)
 			{
 				float l = x * x + y * y;
@@ -81,23 +106,41 @@ namespace Com.Surbon.CSUtils
 				}
 			}
 
+			/// <summary>
+			/// Clamps x between minX and maxX, and y between minY and maxY.
+			/// </summary>
 			public void ClampValues(float minX, float maxX, float minY, float maxY)
 			{
 				x = Clamp(x, minX, maxX);
 				y = Clamp(y, minY, maxY);
 			}
 
+			/// <summary>
+			/// Clamps x and y between min and max.
+			/// </summary>
 			public void ClampValuesUniform(float min, float max)
 			{
 				ClampValues(min, max, min, max);
 			}
 
+			/// <summary>
+			/// Returns the cross product of the vector (this) and the given vector.
+			/// </summary>
 			public float Cross(Vector2 vector) => x * vector.y - y * vector.x;
 
+			/// <summary>
+			/// Returns the distance between the vector (this) and the given vector.
+			/// </summary>
 			public float Distance(Vector2 vector) => MathF.Sqrt((x - vector.x) * (x - vector.x) + (y - vector.x) * (y - vector.y));
 
+			/// <summary>
+			/// Returns the distance squared of the vector (this) and the given vector.
+			/// </summary>
 			public float DistanceSquared(Vector2 vector) => (x - vector.x) * (x - vector.x) + (y - vector.x) * (y - vector.y);
 
+			/// <summary>
+			/// Returns the dot product of the vector (this) and the given vector.
+			/// </summary>
 			public float Dot(Vector2 vector) => x * vector.x + y * vector.y;
 
 			public override bool Equals(object obj)
@@ -107,6 +150,9 @@ namespace Com.Surbon.CSUtils
 				return false;
 			}
 
+			/// <summary>
+			/// Rounds the length of the vector downward.
+			/// </summary>
 			public void FloorLength()
 			{
 				float l = x * x + y * y;
@@ -119,22 +165,43 @@ namespace Com.Surbon.CSUtils
 				}
 			}
 
+			/// <summary>
+			/// Rounds the values of the vector downward.
+			/// </summary>
 			public void FloorValues()
 			{
 				x = MathF.Floor(x);
 				y = MathF.Floor(y);
 			}
 
+			/// <summary>
+			/// Says if the vector is normalized (if the length is equal to 1).
+			/// </summary>
 			public bool IsNormalized() => LengthSquared() == 1f;
 
+			/// <summary>
+			/// Returns the length of the vector.
+			/// </summary>
 			public float Length() => MathF.Sqrt(x * x + y * y);
 
+			/// <summary>
+			/// Returns the length squared of the vector.
+			/// </summary>
 			public float LengthSquared() => x * x + y * y;
 
+			/// <summary>
+			/// Performs a modulus operation on x and y, where the result is in ]-b, 0].
+			/// </summary>
 			public Vector2 NegMod(float mod) => new Vector2(Congruence(x, mod), Congruence(y, mod));
 
+			/// <summary>
+			/// Performs a modulus operation on x and y, where the result is in ]modv.x, 0] for x, and ]modv.y, 0] for y.
+			/// </summary>
 			public Vector2 NegModv(Vector2 modv) => new Vector2(Congruence(x, modv.x), Congruence(y, modv.y));
 
+			/// <summary>
+			/// Sets the length of the vector to the given length.
+			/// </summary>
 			public void Normalize(float length = 1)
 			{
 				float l = x * x + y * y;
@@ -147,6 +214,9 @@ namespace Com.Surbon.CSUtils
 				}
 			}
 
+			/// <summary>
+			/// Returns the normalized vector.
+			/// </summary>
 			public Vector2 Normalized()
 			{
 				float l = LengthSquared();
@@ -158,18 +228,30 @@ namespace Com.Surbon.CSUtils
 				return new Vector2(x / l, y / l);
 			}
 
+			/// <summary>
+			/// Performs a modulus operation on x and y, where the result is in [0, b[.
+			/// </summary>
 			public Vector2 PosMod(float mod) => new Vector2(Congruence(x, mod), Congruence(y, mod));
 
+			/// <summary>
+			/// Performs a modulus operation on x and y, where the result is in [0, modv.x[ for x, and [0, modv.y[ for y.
+			/// </summary>
 			public Vector2 PosModV(Vector2 modv) => new Vector2(Congruence(x, modv.x), Congruence(y, modv.y));
 
-			public Vector2 Rotated(float angle)
+			/// <summary>
+			/// Rotates the vector by phi radians.
+			/// </summary>
+			public Vector2 Rotate(float phi)
 			{
-				float sin = MathF.Sin(angle);
-				float cos = MathF.Cos(angle);
+				float sin = MathF.Sin(phi);
+				float cos = MathF.Cos(phi);
 
 				return new Vector2(x * cos - y * sin, x * sin + y * cos);
 			}
 
+			/// <summary>
+			/// Rounds the length of the vector.
+			/// </summary>
 			public void RoundLength()
 			{
 				float l = x * x + y * y;
@@ -182,15 +264,24 @@ namespace Com.Surbon.CSUtils
 				}
 			}
 
+			/// <summary>
+			/// Rounds the values of the vector.
+			/// </summary>
 			public void RoundValues()
 			{
 				x = MathF.Round(x);
 				y = MathF.Round(y);
 			}
 
+			/// <summary>
+			/// Returns the signs of the vector as (sign x, sign y).
+			/// </summary>
 			public Vector2 Sign() => new Vector2(MathF.Sign(x), MathF.Sign(y));
 		}
 
+		/// <summary>
+		/// Clamps value between min and max.
+		/// </summary>
 		public static float Clamp(float value, float min, float max)
 		{
 			if (value <= min) return min;
@@ -198,14 +289,36 @@ namespace Com.Surbon.CSUtils
 			return value;
 		}
 
+		/// <summary>
+		/// Returns a modulo b.
+		/// </summary>
+		/// <param name="a">The dividend.</param>
+		/// <param name="b">The divisor.</param>
+		/// <param name="isPos">If true, the remainder is negative.</param>
 		public static float Congruence(float a, float b, bool isPos = true) => EuclidianRemainder(a, b) - (isPos ? 0 : b);
 
+
+		/// <summary>
+		/// Returns a modulo b.
+		/// </summary>
+		/// <param name="a">The dividend.</param>
+		/// <param name="b">The divisor.</param>
+		/// <param name="isPos">If true, the remainder is negative.</param>
 		public static int Congruence(int a, int b, bool isPos = true) => EuclidianRemainder(a, b) - (isPos ? 0 : b);
 
+		/// <summary>
+		/// Returns the euclidian quotient of a / b.
+		/// </summary>
 		public static float EuclidianQuotient(float a, float b) => MathF.Floor(a / b);
 
+		/// <summary>
+		/// Returns the euclidian remainder of a / b.
+		/// </summary>
 		public static float EuclidianRemainder(float a, float b) => a - EuclidianQuotient(a, b) * b;
 
+		/// <summary>
+		/// Returns the euclidian remainder of a / b.
+		/// </summary>
 		public static int EuclidianRemainder(int a, int b) => a - (a / b) * b;
 	}
 }
