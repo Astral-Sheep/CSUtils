1c0576ba7a802f4a59ec9c96d075e174c8c23a84
Thomas SURBON <t.surbon@student.isartdigital.com>
2022-07-27 06:54:03 -0400

    feat: add struct Vector2 in class MathT

--- scripts/MathT.cs
+++ scripts/MathT.cs
@@ -7,6 +7,16 @@ namespace Com.Surbon.CSUtils
 	/// </summary>
 	public static class MathT
 	{
+		public struct Vector2
+		{
+			public float x;
+			public float y;
 
+			public Vector2(float pX = 0f, float pY = 0f)
+			{
+				x = pX;
+				y = pY;
+			}
+		}
 	}
 }
