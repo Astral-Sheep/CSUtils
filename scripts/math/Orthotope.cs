43fa2d6cfcb178cc96a425ab8748e33cec4e702f
Thomas SURBON <t.surbon@student.isartdigital.com>
2022-08-26 19:13:28 +0200

    feat: add operators == and != in struct Orthotope

--- scripts/math/Orthotope.cs
+++ scripts/math/Orthotope.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Com.Surbon.CSUtils.Math
 {
-	#pragma warning disable CS0659 // Le type se substitue à Object.Equals(object o) mais pas à Object.GetHashCode()
-	#pragma warning disable CS0661 // Le type définit l'opérateur == ou l'opérateur != mais ne se substitue pas à Object.GetHashCode()
-	#pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
+#pragma warning disable CS0659 // Le type se substitue à Object.Equals(object o) mais pas à Object.GetHashCode()
+#pragma warning disable CS0661 // Le type définit l'opérateur == ou l'opérateur != mais ne se substitue pas à Object.GetHashCode()
+#pragma warning disable CS1591 // Commentaire XML manquant pour le type ou le membre visible publiquement
 
+	/// <summary>
+	/// Representation of a rectangle in N dimensions.
+	/// </summary>
 	public struct Orthotope
 	{
 		public int Dimension => o.Size;
+
+		#region PROPERTIES
 
 		/// <summary>
 		/// The length in the given dimension.
@@ -46,6 +49,8 @@ namespace Com.Surbon.CSUtils.Math
 			}
 		}
 
+		#endregion PROPERTIES
+
 		private VectorN o;
 		private float[] l;
 
@@ -57,5 +62,37 @@ namespace Com.Surbon.CSUtils.Math
 			o = origin;
 			l = lengths;
 		}
+
+		#region OPERATORS
+
+		public static bool operator==(Orthotope orthotope1, Orthotope orthotope2)
+		{
+			if (orthotope1.Dimension != orthotope2.Dimension)
+				return false;
+
+			for (int i = orthotope1.Dimension - 1; i >= 0 ; i--)
+			{
+				if (orthotope1[i] != orthotope2[i])
+					return false;
+			}
+
+			return orthotope1.Origin == orthotope2.Origin;
+		}
+
+		public static bool operator!=(Orthotope orthotope1, Orthotope orthotope2)
+		{
+			if (orthotope1.Dimension != orthotope2.Dimension)
+				return true;
+
+			for (int i = orthotope1.Dimension - 1; i >= 0; i--)
+			{
+				if (orthotope1[i] != orthotope2[i])
+					return true;
+			}
+
+			return orthotope1.Origin != orthotope2.Origin;
+		}
+
+		#endregion OPERATORS
 	}
 }
