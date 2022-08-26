6ee65a9d93a7faf721d817db01c3b7cf4fb4e15e
Thomas SURBON <t.surbon@student.isartdigital.com>
2022-08-26 21:46:12 +0200

    feat: add struct Matrix

---
+++ scripts/math/Matrix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Surbon.CSUtils.Math
+{
+	/// <summary>
+	/// Representation of a matrix
+	/// </summary>
+	public struct Matrix
+	{
+
+	}
+}
