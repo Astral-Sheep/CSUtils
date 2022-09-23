79ba230ba4b310590887eedf72a9cd40e6667a1d
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-09-23 16:03:47 +0200

    feat: add operator * and method GetConjugate in struct Quaternion

--- scripts/math/Quaternion.cs
+++ scripts/math/Quaternion.cs
@@ -33,6 +33,32 @@ namespace Com.Surbon.CSUtils.Math
 			return new Quaternion(q1.a - q2.a, q1.b - q2.b, q1.c - q2.c, q1.d - q2.d);
 		}
 
+		public static Quaternion operator*(Quaternion q1, Quaternion q2)
+		{
+			return new Quaternion(
+				q1.a * q2.a - q1.b * q2.b - q1.c * q2.c - q1.d * q2.d,
+				q1.a * q2.b + q1.b * q2.a + q1.c * q2.d - q1.d * q2.c,
+				q1.a * q2.c + q1.c * q2.a + q1.d * q2.b - q1.b * q2.d,
+				q1.a * q2.d + q1.d * q2.a + q1.b * q2.c - q1.c * q2.b
+				);
+		}
+
+		public static Quaternion operator*(Quaternion q, float lambda)
+		{
+			return new Quaternion(q.a * lambda, q.b * lambda, q.c * lambda, q.d * lambda);
+		}
+
+		public static Quaternion operator*(float lambda, Quaternion q)
+		{
+			return new Quaternion(lambda * q.a, lambda * q.b, lambda * q.c, lambda * q.d);
+		}
+
 		#endregion OPERATORS
+
+		#region INSTANCE
+
+		public Quaternion GetConjugate() => new Quaternion(a, -b, -c, -d);
+
+		#endregion INSTANCE
 	}
 }
