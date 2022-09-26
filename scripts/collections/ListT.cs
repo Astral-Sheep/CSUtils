7444d5ab3f4e55fe5ca9ae086b342315db1d20b1
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-09-26 11:46:31 +0200

    feat: add class ListT

---
+++ scripts/collections/ListT.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Com.Surbon.CSUtils.Collections
+{
+	public static class ListT
+	{
+		public static List<T> Clone<T>(List<T> list) => new List<T>(list);
+	}
+}
